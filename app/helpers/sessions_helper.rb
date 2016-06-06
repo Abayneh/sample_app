ea5c6b8b5469e1646b0336b50f5cf81cdb13f176
Abayneh <tabayneh@gmail.com>
2016-06-06 07:24:52 +0000

    Complete log in with profile view

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,15 @@
+module SessionsHelper
+  #Logs in the given user
+  def log_in(user) 
+    session[:user_id] = user.id # the session method creates a temp cookie in the browser(expires with a closed browser!)
+  end
+  
+  def current_user
+      @current_user ||= User.find_by(id: session[:user_id])
+  end
+  
+  def logged_in?
+    !current_user.nil?
+  end
+  
+end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -15,11 +15,12 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
-      post_via_redirect users_path, user: { name: "Alemnew Chane",
-                                            email: "alexzgreat@yahoo.com",
-                                            password:              "alex1234",
-                                            password_confirmation: "alex1234" }
+      post_via_redirect users_path, user: { name:  "New User",
+                                            email: "new@example.com",
+                                            password:              "password",
+                                            password_confirmation: "password" }
     end
     assert_template 'users/show'
+    assert is_logged_in?
   end
 end
