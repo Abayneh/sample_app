c212ab1166a142f3f37ee4cb98ee1fa9d3bedeb9
Abayneh <tabayneh@gmail.com>
2016-06-06 17:56:31 +0000

    Finishes logout with green test result

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,4 +12,14 @@ module SessionsHelper
     !current_user.nil?
   end
   
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+  
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+  
+  
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -15,15 +15,23 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert flash.empty?
   end
   
-  test "login with valid information" do
+  test "login with valid information followed by logout" do
     get login_path
     post login_path, session: { email: @user.email, password: 'password' }
+    assert is_logged_in?
     assert_redirected_to @user
     follow_redirect!
-    assert_template 'users/show'
-    assert_select "a[href=?]", login_path, count: 0
-    assert_select "a[href=?]", logout_path
-    assert_select "a[href=?]", user_path(@user)
+    assert_template 'users/show' # Show the user info
+    assert_select "a[href=?]", login_path, count: 0 # no more login path
+    assert_select "a[href=?]", logout_path # logout must be present
+    assert_select "a[href=?]", user_path(@user) #user path must be present
+    delete logout_path # (when logout) the logout path must not be present
+    assert_not is_logged_in? # No longer logged in
+    assert_redirected_to root_url #self explanatory
+    follow_redirect! # verify the page is on the root
+    assert_select "a[href=?]", login_path # check login path is present again
+    assert_select "a[href=?]", logout_path, count: 0 # check no more logout path exists
+    assert_select "a[href=?]", user_path(@user), count: 0 # check user info no more exists.
   end
   
 end
