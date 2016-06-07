d3f45ff5c67b7bcba0865a15531f6b0161adcac6
Abayneh <tabayneh@gmail.com>
2016-06-07 20:34:32 +0000

    Unimportant Changes in route and users edit test file

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   post    'login'   => 'sessions#create'
   delete  'logout'  => 'sessions#destroy'
   get     'edit'    => 'users#edit'
-  get     'index'   => 'users#index'
+  # get     'index'   => 'users#index'
   resources :users
 end
   # The priority is based upon order of creation: first created -> highest priority.
