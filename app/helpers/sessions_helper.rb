8594c56b430784d7c6a5db901ccefedca1fd4fa9
Abayneh <tabayneh@gmail.com>
2016-06-07 01:09:45 +0000

    Update, delete users including admin previladge

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -38,5 +38,15 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
+  
+  # Redirects to stored location (or the default)
+  def redirect_back_to(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+  
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   get     'login'   => 'sessions#new'
   post    'login'   => 'sessions#create'
   delete  'logout'  => 'sessions#destroy'
+  get     'edit'    => 'users#edit'
+  get     'index'   => 'users#index'
   resources :users
 end
   # The priority is based upon order of creation: first created -> highest priority.
